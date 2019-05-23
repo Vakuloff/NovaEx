e67b9e7158aa41c187d98dbfd908ded3fd84e834
Nick <nick.vakuloff@gmail.com>
2019-05-22 21:35:32 -0400

    Final Result

---
+++ views/footer.php
@@ -0,0 +1,21 @@
+<footer id=footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 id="madeby">&copy;TechnoAvengers <?php echo date("Y"); ?></h2>
+            </div>
+        </div>
+        <div class="row" align="center">
+            <div class="col-md-12">
+                <a href="../../views/referal/refer.php"><i class="fab fa-centos"></i>Refer</a>
+                <!-- <a href="../../views/contactForm/addContactForm.php"><i class="far fa-address-book"></i>Contact</a> -->
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="../../js/mobile-menu.js"></script>
+<script src="../../js/register.js"></script>
+
+</body>
+
+</html>
